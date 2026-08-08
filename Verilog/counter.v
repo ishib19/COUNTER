d464e2d32f60e7eddae899b2d877b8e551dc0f0f
Ishi Bhardwaj <145827054+ishib19@users.noreply.github.com>
2026-08-08 16:18:20 +0530

    Create counter.v

---
+++ Verilog/counter.v
@@ -0,0 +1,15 @@
+//16 bit Up_Down Counter
+`timescale 1ns/1ps
+module counter(clk,m,rst,count);
+input clk,m,rst;
+output reg [15:0] count;
+always @(posedge clk or negedge rst)
+begin
+if (!rst)
+count=0;
+else if(m)
+count=count+1;
+else
+count=count-1;
+end
+endmodule
